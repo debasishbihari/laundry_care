176b33ee9a9fa44f8a12dd55e4a3bc3b5d3251f0
debasishbihari <debasisbihari287@gmail.com>
2023-06-11 07:33:02 -0700

    routes connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const express = require('express');
 const connect= require('./config/db');
+const cors = require('cors');
+
+const appRoute= require("./routes/app.route");
+const userRoute = require("./routes/user.route");
+const notificationRoute = require("./routes/notification.route");
+const otpRoute = require("./routes/resetpassword.route");
 
 const app = express();
 require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/user",userRoute);
+app.use("/notification",notificationRoute);
+app.use("/otp",otpRoute);
+app.use("/laundry",appRoute);
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to laundry care");
+})
 
 app.listen(process.env.PORT,async()=>{
     await connect();
